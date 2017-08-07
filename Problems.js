08860aebb403fdcd17f0e25628a496aeb9ea780b
ramyatata <Ramyatata007@gmail.com>
2017-08-07 17:17:45 -0500

    Create Problems.js

---
+++ Problems.js
@@ -0,0 +1,68 @@
+/**ToyProblem 3
+ * Given an arbitrary input string, return the first nonrepeated character in
+ * the string. For example:
+ *
+ *   firstNonRepeatedCharacter('ABA'); // => 'B'
+ *   firstNonRepeatedCharacter('AACBDB'); // => 'C'
+ */
+
+/* solution */
+function firstNonRepeatedCharacter(word){
+     var charCount = word.split('').reduce(function(acc, ele, ind){
+         acc.hasOwnProperty(ele) ? acc[ele]++: acc[ele] = 1;
+         return acc;
+     }, {});
+     
+     for(var keys in charCount){
+         if(charCount[keys] === 1){
+             return keys;
+         }
+     }
+}
+
+/* HR Solution */
+
+/** ToyProblem 4
+ * A Fibonacci sequence is a list of numbers that begins with 0 and 1, and each
+ * subsequent number is the sum of the previous two.
+ *
+ * For example, the first five Fibonacci numbers are:
+ *
+ *   0 1 1 2 3
+ *
+ * If n were 4, your function should return 3; for 5, it should return 5.
+ *
+ * Write a function that accepts a number, n, and returns the nth Fibonacci
+ * number. Use a recursive solution to this problem; if you finish with time
+ * left over, implement an iterative solution.
+ *
+ * example usage:
+ * nthFibonacci(2); // => 1
+ * nthFibonacci(3); // => 2
+ * nthFibonacci(4); // => 3
+ * etc...
+ *
+ */
+ 
+ /* solution */
+ /* recursive */
+ var nthFibonacci = function (n) {
+  if ( n <= 0) {
+      return 0;
+  } else if (n === 1) {
+      return 1;
+  } else {  
+      return (nthFibonacci(n-1) + nthFibonacci(n-2));
+  }
+};
+/* Iterative */
+var nthFibonacci = function (n) {
+   var fib = [0, 1];
+   for(var i = 2; i <= n; i ++){
+      fib.push(fib[i-1] + fib[i-2]);
+   }
+   return fib[n];
+};
+
+ /* HR Solution */
+ 
